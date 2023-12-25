44d1370453685002533e327fb64afe71379f09ff
Arpan Das <arpan.das@klythe.com>
2023-12-25 20:16:13 +0530

    todo list created

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,52 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [todo, setTodo] = useState({ id: 0, todoItem: "" });
+  const [todoList, setTodoList] = useState([]);
 
-export default App
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setTodo((values) => ({ ...values, [name]: value }));
+  };
+
+  const addTodo = () => {
+    setTodoList((prevTodoList) => [...prevTodoList, todo]);
+    setTodo({ id: todo.id + 1, todoItem: "" });
+  };
+
+  const deleteTodo=(id)=>{
+    setTodoList((prevTodoList) => prevTodoList.filter((todo) => todo.id !== id));
+  }
+
+  return (
+    <div
+      className="w-100 d-flex "
+      style={{
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <label>Todo-List</label>
+      <div className="d-flex">
+        <input
+          type="text"
+          name="todoItem"
+          value={todo.todoItem}
+          onChange={handleChange}
+        />
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+      {todoList.map((todo) => (
+        <div key={todo.id}>
+          {todo.id}: {todo.todoItem}
+          <button onClick={()=>deleteTodo(todo.id)}>Delete</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
